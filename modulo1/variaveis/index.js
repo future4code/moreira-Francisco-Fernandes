cc354bc118858862422c5b601a7e977f5e9a34eb
FranciscoCleitonFernandes <cleytonsullivan15@gmail.com>
2021-11-29 23:09:15 -0300

    Variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,8 @@
+// Exercícios de interpretação de código
+// Pergunta 1 - 10 5
+// Pergunta 2 - 10 10 10
+// Pergunta 3 -
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
