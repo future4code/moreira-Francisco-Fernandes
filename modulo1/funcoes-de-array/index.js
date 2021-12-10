6da54104bf1e8575bf13b922e1f9000218fcd525
FranciscoCleitonFernandes <cleytonsullivan15@gmail.com>
2021-12-09 23:11:49 -0300

    funcoes-de-array

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,27 @@
+// Exercícios de interpretação de código
+
+// Questão 1
+
+// A
+// Através do map, vai imprimir os parâmentros mome e apelido, além de mostrar o indece que oculpam no array.
+// Também será criado um array com três espaços em branco.
+
+
+// Questão 2
+
+// A
+// Através do map, ele vai imprimir um novo array com os valores do parâmentro nome.
+// A função é invocada através do return, onde ela vai retornar o parâmentro nome.
+// Para assim criar o novoarrayb, como o parâmetro nome, do array usuarios.
+
+
+
+// Questão 3
+
+// A
+// Através do filter, que vai filtrar os resultados em que a função é retornada com o parâmetro negação de apelido.
+// assim, vai ser impresso o item com o parâmetro apelido, cuja o valor não sejá Chijo.
+
+
+
+ // Exercícios de escrita de código
