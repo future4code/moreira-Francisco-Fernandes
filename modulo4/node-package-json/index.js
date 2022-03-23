78578305320d4287fcf36c18c41959d3fc70ce1c
FranciscoCleitonFernandes <cleytonsullivan15@gmail.com>
2022-03-22 23:20:40 -0300

    node e package-json

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,38 @@
+// Exercício 1
+
+/* A)
+Etapa 1: Salve um arquivo como index.js;
+Ex:
+const arguments = process.argv;
+console.log(arguments);
+
+Etapa 2: execute o arquivo index.js
+Ex:
+node index.js
+
+O process.argv contém uma array onde o 0º índice contém o caminho executável, 
+o 1º índice contém o caminho para o arquivo atual e o 
+índice restante contém os argumentos passados.
+*/
+
+// B)
+let nome = prompt("Qual é o seu nome?")
+let idade = Number(prompt("Qual é a sua idade?"))
+console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+
+// C)
+let idadefutura = idade + 7
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idadefutura} anos.`)
+
+
+// Exercício 2
+
+
+
+
+
+
+
+// Exercício 3
+
+// Exercício 4
