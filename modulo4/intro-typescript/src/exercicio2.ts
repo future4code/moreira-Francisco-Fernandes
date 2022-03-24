b762d2ff6976743eaa90161d4576717b4d089353
FranciscoCleitonFernandes <cleytonsullivan15@gmail.com>
2022-03-23 23:48:21 -0300

    introdução ao typescript

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,12 @@
+// Exercício 2
+function imprimeTresCoresFavoritas(
+    cor1: string, 
+    cor2: string, 
+    cor3: string,
+    ) {
+    const arrayDeCores: string[] = []
+
+    arrayDeCores.push(cor1,cor2,cor3) 
+    
+    return arrayDeCores
+}
